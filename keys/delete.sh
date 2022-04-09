db30a2c7913ab85954d48791615fc33ee7ad994f
Shane J <nutratracker@protonmail.com>
2022-04-09 03:21:30 -0400

    copy retired/ scripts into main keys/ folder?

--- keys/delete.sh
+++ keys/delete.sh
@@ -1,24 +1,17 @@
+
 #!/bin/bash
 
 cd $(dirname $0)
-cd keys
 
 # Build string list
-keys=("C0EC6A6F154821E6" "BFAB77B2CB228DC5")
-# retired_keys=("D18E60568103C1CC")  # nested in `keys/retired/` folder.. requires special loop
+keys=$(ls *.pgp)
 keys_str=""
 for key in ${keys[@]}; do
-    keys_str="${keys_str} $key.pgp"
+    keys_str="${keys_str} ${key%.*}"
 done
 # Remove leading whitespace
 keys_str=$(echo $keys_str|xargs)
 
-# Import keys
-printf "\\n\e[1;31m%s\e[0m\\n" "gpg --import --armor $keys_str"
-gpg --import --armor $keys_str
-
-# Sign with your existing trusted key
-for key in ${keys[@]}; do
-    printf "\\n\e[1;31m%s\e[0m\\n" "gpg --sign-key $key"
-    gpg --sign-key $key
-done
+# Delete keys
+printf "\\n\e[1;31m%s\e[0m\\n" "gpg --delete-keys $keys_str"
+gpg --delete-keys $keys_str
--- keys/import.sh
+++ keys/import.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
 cd $(dirname $0)
-keys=$(cat .gpg-id)
-
-cd keys
 
 # Build string list
-# keys=("C0EC6A6F154821E6" "BFAB77B2CB228DC5")
-# retired_keys=("D18E60568103C1CC")  # nested in `keys/retired/` folder.. requires special loop
+keys=$(ls *.pgp)
 keys_str=""
 for key in ${keys[@]}; do
-    keys_str="${keys_str} $key.pgp"
+    keys_str="${keys_str} $key"
 done
 # Remove leading whitespace
 keys_str=$(echo $keys_str|xargs)
@@ -21,6 +17,6 @@ gpg --import --armor $keys_str
 
 # Sign with your existing trusted key
 for key in ${keys[@]}; do
-    printf "\\n\e[1;31m%s\e[0m\\n" "gpg --sign-key $key"
-    gpg --sign-key $key
+    printf "\\n\e[1;31m%s\e[0m\\n" "gpg --sign-key ${key%.*}"
+    gpg --sign-key ${key%.*}
 done
