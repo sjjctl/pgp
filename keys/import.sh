d3581814cba3184a45e3f67574aac4a773bf0b22
Shane J <nutratracker@protonmail.com>
2022-04-09 03:18:51 -0400

    wip

---
+++ keys/import.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd $(dirname $0)
+keys=$(cat .gpg-id)
+
+cd keys
+
+# Build string list
+# keys=("C0EC6A6F154821E6" "BFAB77B2CB228DC5")
+# retired_keys=("D18E60568103C1CC")  # nested in `keys/retired/` folder.. requires special loop
+keys_str=""
+for key in ${keys[@]}; do
+    keys_str="${keys_str} $key.pgp"
+done
+# Remove leading whitespace
+keys_str=$(echo $keys_str|xargs)
+
+# Import keys
+printf "\\n\e[1;31m%s\e[0m\\n" "gpg --import --armor $keys_str"
+gpg --import --armor $keys_str
+
+# Sign with your existing trusted key
+for key in ${keys[@]}; do
+    printf "\\n\e[1;31m%s\e[0m\\n" "gpg --sign-key $key"
+    gpg --sign-key $key
+done
