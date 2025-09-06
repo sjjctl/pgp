fb63af4e64de982639a214c7f94b94d967a728bf
Shane Jaroch <chown_tee@proton.me>
2025-09-06 13:09:40 -0400

    update dual purpose keys

--- scripts/gpg-generate-spec.sh
+++ scripts/gpg-generate-spec.sh
@@ -11,9 +11,9 @@ while true; do
 	KEY_OUTPUT=$(./gpg-generate-ed-sce-keypair.sh)
 	sleep 0.1
 
-	KEY_FLAG_1="$(echo "$KEY_OUTPUT" | grep 'ed25519/[A-Z]')"
-	KEY_FLAG_2="$(echo "$KEY_OUTPUT" | grep '^      [A-Z]\{1\}[A-Z,0-9]\{39\}$')"
-	KEY_FLAG_3="$(echo "$KEY_OUTPUT" | grep 'cv25519/[A-Z]')"
+	KEY_FLAG_1="$(echo "$KEY_OUTPUT" | grep 'ed25519/B')"
+	KEY_FLAG_2="$(echo "$KEY_OUTPUT" | grep '^      B[A-Z,0-9]\{39\}$')"
+	KEY_FLAG_3="$(echo "$KEY_OUTPUT" | grep 'cv25519/B')"
 
 	set -x
 	test "$KEY_FLAG_1" && test "$KEY_FLAG_2" && test "$KEY_FLAG_3" &&
