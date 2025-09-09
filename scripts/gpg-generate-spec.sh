d0b6337ab39b66a969e7e734307f300c85210df2
Shane Jaroch <30691680+gamesguru@users.noreply.github.com>
2025-09-08 21:55:05 -0400

    wip

--- scripts/gpg-generate-spec.sh
+++ scripts/gpg-generate-spec.sh
@@ -1,28 +1,42 @@
 #!/bin/bash
 
-# set -x
+# Change to script's directory
+cd "$(dirname "$0")"
 
-# while [[ ! ("$(grep 'rsa4096/0BE' output.log)" || true) ]]; do
-# 	# ./gpg-generate-rsa4096-encryption.sh | tee output.log;
-# 	./gpg-generate-ed-sce-keypair.sh;
-# done
+function check_matchnumber() {
+
+	if [ "$#" -gt $1 ]; then
+		echo "Matched spec! DONE!"
+		exit
+	fi
+}
 
 while true; do
 	KEY_OUTPUT=$(./gpg-generate-ed-sce-keypair.sh)
 	sleep 0.1
 
-	KEY_FLAG_1="$(echo "$KEY_OUTPUT" | grep 'ed25519/B')"
-	KEY_FLAG_2="$(echo "$KEY_OUTPUT" | grep '^      B[A-Z,0-9]\{39\}$')"
-	KEY_FLAG_3="$(echo "$KEY_OUTPUT" | grep 'cv25519/B')"
+	# SPEC: All three (fpr, key_id, and subkey_id) start with a given letter, i.e., B
+	# MATCH_1="$(echo "$KEY_OUTPUT" | grep 'ed25519/E' | cut -c 1-16)"
+	# MATCH_2="$(echo "$KEY_OUTPUT" | grep '^      E[A-Z,0-9]\{39\}$' | cut -c 1-16)"
+	# MATCH_3="$(echo "$KEY_OUTPUT" | grep 'cv25519/E' | cut -c 1-16)"
+	# set -x
+	# check_matchnumber 3 $MATCH_1 $MATCH_2 $MATCH_3
+	# set +x
+	# -- END SPEC --
 
+	# All three start with at least four numbers
+	MATCH_1="$(echo "$KEY_OUTPUT" | grep 'ed25519/[0-9]\{4\}$' | cut -c 1-16)"
+	MATCH_2="$(echo "$KEY_OUTPUT" | grep '^      E[A-Z,0-9]\{39\}$' | cut -c 1-16)"
+	MATCH_3="$(echo "$KEY_OUTPUT" | grep 'cv25519/E' | cut -c 1-16)"
 	set -x
-	test "$KEY_FLAG_1" && test "$KEY_FLAG_2" && test "$KEY_FLAG_3" &&
-		echo "Matched spec! DONE!" &&
-		break
+	check_matchnumber 3 "$MATCH_1" "$MATCH_2" "$MATCH_3"
 	set +x
+	# -- END SPEC --
 
-	# if [[  "$KEY_FLAG_1" && "$KEY_FLAG_2" && "$KEY_FLAG_3" ]]; then
-	# 	echo "Matched spec! DONE!"
-	# 	break
-	# fi
 done
+
+# OLD
+# while [[ ! ("$(grep 'rsa4096/0BE' output.log)" || true) ]]; do
+# 	# ./gpg-generate-rsa4096-encryption.sh | tee output.log;
+# 	./gpg-generate-ed-sce-keypair.sh;
+# done
