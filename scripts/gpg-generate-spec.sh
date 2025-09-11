5c250bb5f4396c3ab59f9b302f43f7ff570eb3ca
Shane <30691680+gamesguru@users.noreply.github.com>
2025-09-11 07:33:26 -0400

    update

--- scripts/gpg-generate-spec.sh
+++ scripts/gpg-generate-spec.sh
@@ -24,11 +24,11 @@ while true; do
 	sleep 0.1
 
 	# SPEC: All four (key_id & fpr, and subkey_id & subkey_fpr) start with a given letter, i.e., B
-	L=E
-	MATCH_1="$(echo "$KEY_OUTPUT" | grep "ed25519/$L" | awk '{print $2}')"
-	MATCH_2="$(echo "$KEY_OUTPUT" | grep -m1 "^      $L[A-Z,0-9]\{39\}$" | cut -c 7-16)"
-	MATCH_3="$(echo "$KEY_OUTPUT" | grep "cv25519/$L" | awk '{print $2}')"
-	MATCH_4="$(echo "$KEY_OUTPUT" | grep -m2 "^      $L[A-Z,0-9]\{39\}$" | tail -n1 | cut -c 7-16)"
+	L=C
+	MATCH_1="$(echo "$KEY_OUTPUT" | grep -m1 "ed25519/$L" | awk '{print $2}')"
+	MATCH_2="$(echo "$KEY_OUTPUT" | grep -m1 "$L[A-Z,0-9]\{39\}$" | tail -n1 | cut -c 7-18)"
+	MATCH_3="$(echo "$KEY_OUTPUT" | grep -m1 "cv25519/$L" | awk '{print $2}')"
+	MATCH_4="$(echo "$KEY_OUTPUT" | grep -m2 "$L[A-Z,0-9]\{39\}$" | grep -v "$MATCH_2" | cut -c 7-18)"
 	check_matchnumber 4 "$MATCH_1" "$MATCH_2" "$MATCH_3" "$MATCH_4"
 	# -- END SPEC --
 
